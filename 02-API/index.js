aa8452969eea929c4c1841c87867e91f72871abb
Gerardo Hernandez Diaz <hernandezdiaz@MacBook-de-Hernandez-Diaz.local>
2020-05-28 01:11:20 -0500

    Clase 02 Cinta negra, ejercicios API

--- 02-API/index.js
+++ 02-API/index.js
@@ -15,13 +15,13 @@ server.get('/api/', (req, res) => {
     res.send({
         mensaje: 'Hola mundo'
     })
-})
+});
 
 //Ejercicio 1
 server.get('/api/suma/', (req, res) => {
    const {num1, num2} = req.query
    res.status(200).json({ resultado: ( parseInt(num1) + parseInt(num2) )})
-})
+});
 
 //Ejercicio 3
 server.get('/api/usuario/:usuario', (req, res) => {
@@ -30,26 +30,28 @@ server.get('/api/usuario/:usuario', (req, res) => {
     res.send({
         usuario: req.params.usuario
     })
-})
+});
 
 //Ejercicio 4
-server.get('/api/swapi/:id', (req, res) => {
+server.get('/api/swapi/:id', (request, response) => {
     //peticion con request
-    const { id } = req.params;
+    const { id } = request.params;
     const SWAPI_URI = `https://swapi.dev/api/people/${id}`;
 
     axios.get(SWAPI_URI)
-    .then(axiosRes => {
-        const personaje = res.data;
-        axiosRes.json({ personaje })
+    .then(axiosResponse => {
+        const personaje = axiosResponse.data;
+       response.json({ personaje })
     })    
-    .catch(console.log(err));
+    .catch(error => {
+        response.status(400).json({error});
+    });
    
-})
+});
 
 //POST
 server.post('/perfil', (req, res) => {
     res.send('Hola desde post');
-})
+});
 
 server.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
