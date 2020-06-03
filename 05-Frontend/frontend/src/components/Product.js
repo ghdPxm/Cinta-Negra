9a2e4490680651c4591db3b37cd921d5eba0338d
Gerardo Hernandez Diaz <hernandezdiaz@MacBook-de-Hernandez-Diaz.local>
2020-06-02 20:45:54 -0500

    Renderizar Products con props

---
+++ 05-Frontend/frontend/src/components/Product.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ProductsList from './ProductsList';
+
+const Product = (props) => {
+    return(
+        <div style={{ 
+            padding: "15px", 
+            backgroundColor: "navy", 
+            color: "white",
+            width: "300px",
+            textAlign: "center",
+            border: "1.5px solid gray",
+            borderRadius: "5px",
+            marginBottom: "10px" }}>
+            
+            <h3>{props.name}</h3>
+            <h4>{`$ ${props.price}`}</h4>
+        </div>
+    );
+}
+
+export default Product;
--- 05-Frontend/frontend/src/components/ProductsList.js
+++ 05-Frontend/frontend/src/components/ProductsList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Product from './Product'
 
 const ProductsList = () => {
     return(
         <React.Fragment>
             <h2>Products</h2>
+            <Product 
+                name="Sopa Nissin"
+                price = {10.50} 
+            />
             <span>My product :P</span>
         </React.Fragment>
     );
