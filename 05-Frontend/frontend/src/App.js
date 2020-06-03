e1ac7ec87e2a449425e2dbe18842204efce1e036
Gerardo Hernandez Diaz <hernandezdiaz@MacBook-de-Hernandez-Diaz.local>
2020-06-02 20:31:16 -0500

    Estructura inicial del proyecto

--- 05-Frontend/frontend/src/App.js
+++ 05-Frontend/frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import ProductsList from './components/ProductsList';
 function App() {
   return (
     <div>
       <h1>Hola mundo</h1>
-    
+      <ProductsList />
     </div>
   );
 }
---
+++ 05-Frontend/frontend/src/components/ProductsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProductsList = () => {
+    return(
+        <React.Fragment>
+            <h2>Products</h2>
+            <span>My product :P</span>
+        </React.Fragment>
+    );
+}
+
+export default ProductsList;
